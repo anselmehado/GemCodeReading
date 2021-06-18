9e1cd2b4b9aa8a167c054d6c3dcbb8f89bf3f3bc
hado anselme <anselmehado85@yahoo.com>
2021-06-18 16:16:27 +0100

    fail1

--- lib/slack-notify/connection.rb
+++ lib/slack-notify/connection.rb
@@ -2,7 +2,7 @@ module SlackNotify
   module Connection
     def send_payload(payload)
       #conn = Faraday.new(@webhook_url) do |c|
-      conn = Faraday.new(https://hooks.slack.com/services/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) do |c|
+      conn = Faraday.new(https://hooks.slack.com/services/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) do |c|
         c.use(Faraday::Request::UrlEncoded)
         c.adapter(Faraday.default_adapter)
         c.options.timeout      = 5
